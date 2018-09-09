f0337bd94a8b8966765a76a69a502e0d85a87437
Snazzy <stankovicnino@gmail.com>
2018-09-09 05:57:05 +0200

    gallery update

--- js/main.js
+++ js/main.js
@@ -86,9 +86,37 @@ $('a[href*="#"]')
     });
 
 // ------------------
-// PRELOADER
+// GALLERY
 // ------------------
 
-$(window).on('load', function () {
-    $('.preloader').addClass('complete')
-})
+var slides = $('.gallery__slide');
+
+var flag = 0;
+
+slides.first().before(slides.last())
+
+setInterval(show, 4000);
+
+function show() {
+    slides = $('.gallery__slide');
+    var activeSlide = $('.active');
+
+    slides.last().after(slides.first());
+
+    activeSlide.removeClass('active').next('.gallery__slide').addClass('active');
+
+    if (flag == 0) {
+        $('.box').css({ '-webkit-clip-path': 'polygon(0 100%, 100% 100%, 100% 90%, 85% 95%, 10% 78%, 7% 11%, 90% 5%, 85% 95%, 100% 95%, 100% 0, 0 0, 0 100%' });
+        flag = 1;
+    } else {
+        $('.box').css({
+            '-webkit-clip-path': 'polygon(0 100%,100% 100%,100% 80%,93% 85%,8% 95%,15% 6%,89% 22%,93% 85%,100% 80%,100% 0,0 0,0 100%)'
+        });
+        flag = 0;
+    }
+}
+
+
+
+
+
